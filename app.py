889ee7885c952031bf818b5f4e1ab9e9264cf23a
shibatahan <shiashib@gmail.com>
2025-08-11 10:27:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,1 @@
-C:\Users\shias\OneDrive\Desktop\ドキュメント\【練習】python\web\app.py
-C:\Users\shias\OneDrive\Desktop\ドキュメント\【練習】python\web\requirements.txt
-C:\Users\shias\OneDrive\Desktop\ドキュメント\【練習】python\web\sample.jpg
-C:\Users\shias\OneDrive\Desktop\ドキュメント\【練習】python\web\pokeapi250724.csv
+
